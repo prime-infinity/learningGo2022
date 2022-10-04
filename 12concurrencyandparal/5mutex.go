76fecf192d4b7250467a44f72c6a78ef1c1b74c7
prime-infinity <soe.a.prime@gmail.com>
2022-10-04 15:13:30 -0700

    started mutexes

---
+++ 12concurrencyandparal/5mutex.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+	to oversimplify, a mutex provides a way to
+	lock certain parts of the application
+
+	this becomes necessary when performing concurrency,
+	and different goroutines are trying to access or modify
+	certain parts of the application
+
+	consider the example below
+*/
+
+func main() {
+
+}
